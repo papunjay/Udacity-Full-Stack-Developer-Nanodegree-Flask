e50fbdd053ffab8c8c657c1754fe99e2fbd168b5
papunjay <kumarpapunjay@gmail.com>
2020-10-16 13:44:23 +0530

    [papunjay].Add function index

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 import flask from Flask,render_template
 app = Flask(__name__)
-
+def index():
+    return render_template('index.html',data[
+        {
+            description:'todo1'
+        },
+        {
+            description:'todo2'
+        },
+        {
+            description:'todo3'
+        },
+        {
+            description:'todo4'
+        }
+    ])
